465ff3ffbce1cd32d925a616efa8b926f2f771ee
jaz <70194414+Jaz123-rgb@users.noreply.github.com>
2023-11-09 19:29:50 -0600

    update

--- app/authentication/password_utils.py
+++
@@ -1,9 +0,0 @@
-# hashing.py
-
-from passlib.context import CryptContext
-
-# Crea una instancia de la clase CryptContext para manejar contraseñas
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
